8201d5e21e6bfeac65ff6d672fb775b7992cdab1
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-01 20:05:16 -0400

    Cleaning up Photogur

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,18 @@
+class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def create
+    render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
+  def new
+  end
+
+end
